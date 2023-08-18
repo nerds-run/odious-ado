31ff8c03be41bf7da3de4d0f59cf53b3c076075b
abanna <alexanderbanna@gmail.com>
2023-08-18 02:18:07 -0500

    whateva

--- odious_ado/plugins/ado/__init__.py
+++ odious_ado/plugins/ado/__init__.py
@@ -2,4 +2,30 @@ from odious_ado.plugins.ado.client import AdoClient
 from odious_ado.plugins.ado.state import *
 from odious_ado.plugins.ado.effort import *
 
+<<<<<<< HEAD
 __all__ = ["AdoClient", "get_ADO_state", "set_ADO_state", "get_ADO_effort", "set_ADO_effort"]
+=======
+
+__all__ = ["AdoClient"]
+
+
+#
+# when issue is created do ms shit - almost done - alex will finish
+#
+# when this issue is create and what not ado stuff
+#
+# - this stuff comes from .github/stupid_action.yaml
+#
+
+
+# mr github create issues (maybe based off pr ) -
+# when
+
+
+
+# ADO -- must get updated when a github issue is created. (when isssue created make new thing in ado)
+# AD0 -- comments should be updated when issue comments are made (from github when some comment is made on the issue
+# update ado)
+# ADO -- when any updates happen make use to add Github or / / issue links
+# ADO is just microsoft --- death
+>>>>>>> 944fe27 (smeowmix smowmix lots of fun)
